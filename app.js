c592fe67c417ac456396e36d0953a895f1f67ebc
Willian <willianwda.2@gmail.com>
2022-06-27 11:46:51 -0300

    Adicionando console log

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log ("Rodando o sistema de gerenciamento de jogos")
+console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("Alpicação está se conectando ao banco de dados")
+// mais um console log de teste
+console.log("Commit teste")
+console.log("Enviando para o GITHUB")
